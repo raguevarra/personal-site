f91bd0b428ef8ce134d42a8e4b961f2648d41cf4
raguevarra <122047688+raguevarra@users.noreply.github.com>
2026-07-05 23:03:46 -0400

    layout for pages
    reusable layout for pages

---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from "react";
+import { Box, Container } from "@chakra-ui/react";
+import Navbar from "./Navbar";
+
+type LayoutProps = {
+    children: ReactNode;
+};
+
+function Layout({ children }: LayoutProps) {
+    return (
+        <Box minH="100vh" bg="#fbf9ff" color="#24213a">
+            <Container maxW="7x1" py={6}>
+                <Navbar />
+                <Box as="main">{children}</Box>
+            </Container>
+        </Box>
+    );
+}
+
+export default Layout;
