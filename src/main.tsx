24ba8c4b54c6735f02fdf6c16f0afa2e94ec7812
raguevarra <122047688+raguevarra@users.noreply.github.com>
2026-07-05 23:52:46 -0400

    updated main.tsx
    wrapped in a browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider value={defaultSystem}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
