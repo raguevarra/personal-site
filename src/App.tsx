68e962632805ee8267fde4f4f4b25932e1c4cfba
raguevarra <122047688+raguevarra@users.noreply.github.com>
2026-07-07 22:26:38 -0400

    updated app.tsx
    added experience route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
       </Routes>
     </Layout>
   );
